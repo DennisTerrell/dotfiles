86d790e915e38869c56ccdb0c78fd72c4310a4cc
dennis <dennisterrell@gmail.com>
2018-04-28 19:50:16 -0700

    Added checks on shell load

--- profile.ps1
+++ profile.ps1
@@ -1,5 +1,14 @@
 $env:Path = "C:\Program Files\Git\bin;C:\Program Files\Git\usr\bin;C:\Windows\system32\;E:\Hashicorp\bin\;C:\Windows\System32\OpenSSH\;"
 
+if (-Not(Test-Path -Path "C:\ProgramData\Chocolatey")) {
+   iwr https://chocolatey.org/install.ps1 -UseBasicParsing | iex
+}
+if (-Not(Test-Path -Path "C:\Program Files\PackageManagement\ProviderAssemblies\nuget")) {
+  Install-PackageProvider NuGet -MinimumVersion '2.8.5.201' -Force
+}
+
+Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
+
 Import-Module Get-ChildItemColor
 Import-Module -Name posh-git
 Import-Module -Name oh-my-posh
